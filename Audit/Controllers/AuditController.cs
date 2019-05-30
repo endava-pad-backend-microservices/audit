549feb5218e2d0645801ce199b1ce303437a01ce
Jose Miguel Solaro <Jose.Solaro@endava.com>
2019-05-30 10:12:09 -0300

    Add basic audit configuration

--- Audit/Controllers/AuditController.cs
+++ Audit/Controllers/AuditController.cs
@@ -14,34 +14,34 @@ namespace Audit.Controllers
     {
         // GET: Audit
         [HttpGet]
-        public async Task<JsonResult> Get()
+        static public async Task<JsonResult> Get()
         {
             return new JsonResult(await new Repository.AuditRepository().FindAll().ConfigureAwait(true));
         }
         // GET: Audit/id
         [HttpGet("{id}")]
-        public async Task<JsonResult> GetbyId(string id)
+        static public async Task<JsonResult> GetbyId(string id)
         {
             return new JsonResult(await new Repository.AuditRepository().FindById(id).ConfigureAwait(true));
         }
 
         // POST: Audit
         [HttpPost]
-        public async Task<JsonResult> Post([FromBody] Entity.Audit value)
+        static public async Task<JsonResult> Post([FromBody] Entity.Audit value)
         {
             return new JsonResult(await new Repository.AuditRepository().Create(value).ConfigureAwait(true));
         }
 
         // POST: Audit/id
         [HttpPut]
-        public async Task<JsonResult> Put([FromBody] Entity.Audit value)
+        static public async Task<JsonResult> Put([FromBody] Entity.Audit value)
         {
             return new JsonResult(await new Repository.AuditRepository().Update(value).ConfigureAwait(true));
         }
 
         // DELETE: Audit/5
         [HttpDelete("{id}")]
-        public async Task<bool> Delete(string id)
+        static public async Task<bool> Delete(string id)
         {
             var repo = new Repository.AuditRepository();
             var doc = await repo.FindById(id,false).ConfigureAwait(true);
